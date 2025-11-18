d9cfb7ad22cb1e3d43d65efc7d05a137ee213864
Andej Horny <horny.andrej@dpb.sk>
2025-11-18 11:12:25 +0100

    first commit

---
+++ Infrastructure/Persistence/Eloquent/Models/EloquentTaskGroup.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dpb\Package\TaskMS\Infrastructure\Eloquent\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EloquentTaskGroup extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'code',
+        'title',
+        'description',
+        'parent_id',
+    ];
+
+    public function getTable()
+    {
+        return config('pkg-task-ms.tasks_table_prefix') . 'task_groups';
+    }    
+
+    // public function tasks(): HasMany
+    // {
+    //     return $this->hasMany(Task::class, "group_id");
+    // }
+
+    // public function parent(): BelongsTo
+    // {
+    //     return $this->belongsTo(TicketGroup::class, "parent_id");
+    // }    
+
+    public function scopeByCode(Builder $query, string|array $code)
+    {
+        // cast input to array
+        $code = is_array($code) ? $code : [$code];
+
+        $query->whereIn('code', $code);        
+    }    
+}
